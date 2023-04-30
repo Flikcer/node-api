9fbe164ce100b6ef57d54520d211148735b46c60
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-04-30 14:53:10 -0400

    conected to test mongo db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 //routes
 
@@ -7,6 +8,18 @@ app.get("/", (req, res) => {
   res.send("Hello Node API");
 });
 
-app.listen(3000, () => {
-  console.log("Node API running");
+app.get("/blog", (req, res) => {
+  res.send("Hello Blog");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:password1224@cluster0.ujbgaqw.mongodb.net/node-api?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to mongo");
+    app.listen(3000, () => {
+      console.log("Node API running");
+    });
+  })
+  .catch(() => console.log(error));
