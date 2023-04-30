4eaa204d86b9ec02a10dfb275f8f88222a6e5774
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-04-30 16:00:19 -0400

    all middleware added to control JSON in DB

--- server.js
+++ server.js
@@ -16,10 +16,49 @@ app.get("/blog", (req, res) => {
   res.send("Hello Blog");
 });
 
-app.post("/product", async (req, res) => {
+//get products from db in json
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const id = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
+  } catch (error) {
+    alert(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//update a product
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    //no product found to update
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find product with id ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
   } catch (error) {
     alert(error.message);
     res.status(500).json({ message: error.message });
