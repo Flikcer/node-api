c6853000114ef50c04e0cdf361e1e3821e0f9759
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-04-30 16:04:33 -0400

    finalized template with delete function

--- server.js
+++ server.js
@@ -65,6 +65,23 @@ app.put("/products/:id", async (req, res) => {
   }
 });
 
+//delete product
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find product with id ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    alert(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 mongoose.set("strictQuery", false);
 mongoose
   .connect(
